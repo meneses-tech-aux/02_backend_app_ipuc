1e478e605914e2baf58b4aef10878f67e0324b77
Frey Meneses <fmenesest@pucp.edu.pe>
2026-08-17 09:56:53 -0500

    Deploy sh apuntando al dev de preprod v3

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ echo "DEPLOY SH -> ACTUALIZANDO SERVIDOR"
 
 git pull origin main
 
-sudo docker compose -f docker-compose.dev.yml up -d --build
+sudo docker compose --env-file .env.preproduccion -f docker-compose.dev.yml up -d
 
 echo "DEPLOY SH -> DESPLIEGUE FINALIZADO"
