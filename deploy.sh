028549105af743e204ed836741b829be00dd7f32
Frey Meneses <fmenesest@pucp.edu.pe>
2026-08-19 06:12:45 -0500

    APIRest IPUCP Mobile

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd /home/ubuntu/02_backend_app_ipuc/
 git pull origin main
 
 # 3. Levantar los contenedores FORZANDO la reconstrucción de la imagen de la API
-sudo docker compose --env-file .env.development -f docker-compose.dev.yml up -d --build
+sudo docker compose --env-file .env.development -f docker-compose.dev.yml up -d --build --force-recreate
 
 # 4. Limpiar imágenes viejas para que tu servidor AWS no se quede sin espacio de disco con el tiempo
 sudo docker image prune -f
